320689db026749e1447cfc306f4d21fa616da389
souhub <ace0804sou@gmail.com>
2021-04-05 11:58:23 +0900

    Updated server

--- server.py
+++ server.py
@@ -9,19 +9,23 @@ app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/recommendation', methods=['GET'])
-def inputted_data():
+def recommendation():
     data = request.args.get("selected_wemen_ids")
     list_data = data.split(',')
     # list_data: dataをint型の配列に変換したもの
     list_data = [int(i) for i in list_data]
 
     # AI の処理
-    outputs = recommend(list_data)
+    try:
+        outputs = recommend(list_data)
+    except:
+        outputs = [[0], [0], [0]]
+        print("AIアルゴリズムのエラー")
     # ここまでAIの処理
 
     # outputs を recommended_actresses に格納
     recommended_actresses = {
-        "actresses_ids": outputs[0],
+        "recommended_actresses_ids": outputs[0],
         "states": outputs[1],
         "epsilons": outputs[2]
     }
@@ -31,4 +35,4 @@ def inputted_data():
 
 # export FLASK_APP=server.py + flask run または python3 server.py で起動
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
